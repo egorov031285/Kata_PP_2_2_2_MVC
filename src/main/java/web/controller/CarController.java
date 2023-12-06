bb39558067e4b05c79ed8cf4c1e081199dc4226f
egorov031285 <egorov031285@gmail.com>
2023-12-06 16:23:53 +0300

    v1.1

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -20,7 +20,7 @@ public class CarController {
     public String getAllCars(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
         model.addAttribute("cars", carService.getCars(count));
 
-        System.out.println(carService.getCars(count).toString());
+        //System.out.println(carService.getCars(count).toString());
 
         return "cars";
     }
